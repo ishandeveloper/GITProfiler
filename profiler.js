8364fef007ececc78d8deeafdf6de71b21c49b4d
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-17 21:24:41 +0530

    Added Stats

--- profiler.js
+++ profiler.js
@@ -40,32 +40,7 @@ function profileClick(){
             });
         });
         $('#results').html(`
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">${user.name}</h3>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <img class="thumbnail avatar" src="${user.avatar_url}">
-                            <a target="_blank" class="btn btn-primary btn-block viewbtn" href="${user.html_url}">View Profile</a>
-                        </div>
-                        <div class="col-md-9">
-                            <h3 class="badges"><span class="badge badge-primary">Public Repos: ${user.public_repos}</span></h3>
-                            <h3 class="badges"><span class="badge badge-dark">Public Gists: ${user.public_gists}</span></h3>
-                            <h3 class="badges"><span class="badge badge-success">Followers: ${user.followers}</span></h3>
-                            <h3 class="badges"><span class="badge badge-info">Following: ${user.following}</span></h3>
-                            <br><br>
-                            <ul class="list-group">
-                                <li class="list-group-item">Bio: ${user.bio}</li>
-                                <li class="list-group-item">Company: ${user.company}</li>
-                                <li class="list-group-item">Location: ${user.location}</li>
-                                <li class="list-group-item">Member Since: ${user.created_at.split('T')[0]}</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>  
+            <div class="">  
             <br><br>
             <h3 class="page-header">Latest Repos</h3>
             <br>
